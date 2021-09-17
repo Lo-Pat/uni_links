d38592a0d00022ce804322d9ab585c20d4bd7f07
Lo_pat <Lo_pat@qq.com>
2021-09-17 16:23:42 +0800

    fix:initialLink

--- uni_links/ios/Classes/UniLinksPlugin.m
+++ uni_links/ios/Classes/UniLinksPlugin.m
@@ -45,8 +45,8 @@ static id _instance;
   [self didChangeValueForKey:key];
 
   if (_eventSink) {
-      _eventSink(_latestLink)
-      self.initialLink = nil
+      _eventSink(_latestLink);
+      self.initialLink = nil;
   };
 }
 
@@ -82,7 +82,7 @@ static id _instance;
 - (void)handleMethodCall:(FlutterMethodCall *)call result:(FlutterResult)result {
   if ([@"getInitialLink" isEqualToString:call.method]) {
     result(self.initialLink);
-    self.initialLink = nil
+    self.initialLink = nil;
     // } else if ([@"getLatestLink" isEqualToString:call.method]) {
     //     result(self.latestLink);
   } else {
