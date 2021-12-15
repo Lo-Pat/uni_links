83689bf7c84371a9ceb983ce8f0cc31e71036914
Lo_pat <Lo_pat@qq.com>
2021-12-15 15:37:52 +0800

    add:openUrl

--- uni_links/ios/Classes/UniLinksPlugin.h
+++ uni_links/ios/Classes/UniLinksPlugin.h
@@ -5,6 +5,9 @@ NS_ASSUME_NONNULL_BEGIN
 @interface UniLinksPlugin : NSObject <FlutterPlugin>
 + (instancetype)sharedInstance;
 - (BOOL)application:(UIApplication *)application
+            openURL:(NSURL *)url
+            options:(NSDictionary<UIApplicationOpenURLOptionsKey, id> *)options;
+- (BOOL)application:(UIApplication *)application
     continueUserActivity:(NSUserActivity *)userActivity
       restorationHandler:(void (^)(NSArray *_Nullable))restorationHandler;
 @end
